3a84bae8b74a412f88780c13438d3fcc1ad38a5c
sbx_user1051 <sbx_user1051@169.254.28.49>
2025-12-06 17:42:38 +0000

    Sat Dec 06 17:42:38 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,5 +6,10 @@ class CheckoutSolution:
         price = {
             "A": 50, "B": 30, "C": 15
         }
+        offers = {"A": (3,130), "B": (2,45)}
 
-        
+        counts = {"A": 0, "B": 0, "C": 0}
+        for sku in skus:
+            counts[sku] += 1
+        for item, qty in counts.items():
+            
